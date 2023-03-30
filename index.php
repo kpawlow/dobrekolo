a0033fc95c610eea33c84e34e1a2ba62be20769c
kpawlow <krzysztof.pawlow@gmail.com>
2023-03-30 13:10:06 +0200

    Główny plik strony

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strona Dobre Koło</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <div id="gora">Tutaj będzie menu</div>
+    <div id="zawartosc">Tutaj będzie zawartość strony</div>
+    <div id="stopka">Stronę wykonał Krzysztof Pawłowski</div>
+</body>
+</html>
